584f6e2a13adba54e78d5c38ac638daf821d13d0
Tiana0117 <yining0117@g.ucla.edu>
2021-10-29 20:35:28 -0700

    css and logistics updating

--- app.py
+++ app.py
@@ -12,29 +12,30 @@ import string
 
 app = Flask(__name__)
 
-def get_messsage_db():
+def get_message_db():
 	if 'message_db' not in g:
 		g.message_db = sqlite3.connect("messages_db.sqlite")
-		cmd = \
-		"""
-		CREATE TABLE IF NOT EXISTS 'message' (id INTEGER, handle TEXT, message TEXT)
-		"""
-		curseur = g_message_db.cursor()
-		curseur.execute(cmd)
+	cmd = \
+	"""
+	CREATE TABLE IF NOT EXISTS messages (id INT, handle TEXT, message TEXT)
+	"""
+	cursor = g.message_db.cursor()
+	cursor.execute(cmd)
 
 	return g.message_db
 
 def insert_message(request):
-	if request.method == 'POST':
-		message = request.form['message']
-		handle = request.form['handle']
+	if request.method == "POST":
+		message = request.form["message"]
+		handle = request.form["handle"]
 
-		cmd_2 = \
-		"""
-		INSERT INTO messages (id, handle, message) VALUES (str(number_of_rows[0]+1), handle, message)
-		"""
+		
 		cursor = get_message_db().cursor()
-		cursor.execute(cmd_2)
+		cursor.execute("SELECT COUNT(*) FROM messages")
+		row_num = cursor.fetchone()
+
+		
+		cursor.execute("INSERT INTO messages (id, handle, message) VALUES (" + str(row_num[0]+1) + ", \"" + handle + "\", \"" + message + "\")")
 		get_message_db().commit()
 		get_message_db().close()
 		# db = get_message_db()
@@ -45,7 +46,6 @@ def random_messages(n):
 	cursor = get_message_db().cursor()
 	cursor.execute("SELECT * FROM messages ORDER BY RANDOM() LIMIT " + str(n))
 	random_messages = cursor.fetchall()
-	# print(random_messages)
 	get_message_db().close()
 	return random_messages
 
@@ -55,12 +55,12 @@ def random_messages(n):
 
 @app.route("/")
 def main():
-	return render_template("base.html")
+	return render_template('base.html')
 
 
 @app.route("/submit/", methods = ["POST", "GET"])
 def submit():
-	if request.method == 'GET':
+	if request.method == "GET":
 		return render_template('submit.html')
 	else:
 		try:
@@ -71,8 +71,7 @@ def submit():
 
 @app.route("/view/")
 def view():
-	random_messages(5)
-	return render_template("view.html")
+	return render_template("view.html",random_messages = random_messages(5))
 
 
 
